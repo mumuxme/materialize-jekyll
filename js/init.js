5b8513d13d9fc8426ea86f482947b790ac4d8198
mumu <x913060109@gmail.com>
2017-09-17 22:35:32 +0800

    make materialize constrainWidth true

--- js/init.js
+++ js/init.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
   $('.post-toc').dropdown({
     inDuration: 300,
     outDuration: 225,
-    constrainWidth: false,
+    constrainWidth: true,
     hover: true,
     gutter: 0,
     belowOrigin: false,
